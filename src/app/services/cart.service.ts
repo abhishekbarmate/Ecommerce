51686eb59663ef755fc09915d54ab3462ec81d47
abhishekbarmate <abhishekbarmate19@gmail.com>
2024-03-08 17:46:15 +0530

    cart details frontend completed

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -15,12 +15,11 @@ export class CartService {
   constructor() { }
 
   addToCart(theCartItem: CartItem) {
-    console.log("cart items --"+ theCartItem);
+
     // check if we already have the item in our cart
     let alreadyExistsInCart: boolean = false;
     let existingCartItem: CartItem = undefined;
-   
-   
+
     if (this.cartItems.length > 0) {
       // find the item in the cart based on item id
 
@@ -72,4 +71,30 @@ export class CartService {
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('----');
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    }
+    else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+  }
+
 }
